efb701fc31bdbfa2b14d668529571f7d36924276
Devon R <sansgorialis@gmail.com>
2020-12-13 01:54:32 +0900

    GitHub Actions workflow for building wheels

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,6 @@
 curl --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 
 if command -v yum; then
-    yum install -y openssl-devel opus
+    # We must set this option otherwise yum will just continue silently if it can't install one of these
+    yum --setopt=skip_missing_names_on_install=False install -y openssl-devel opus
 fi
